39af8e7e91cfe25750c8f00694f16f9ebcd2e4cf
Gabriel Meyer <ghmeyer0@gmail.com>
2020-05-26 01:43:18 -0300

    Implementado funções para pegar a localização do mapa

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,20 +1,56 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import MapView from 'react-native-maps';
-import { StyleSheet} from 'react-native';
+import { Marker } from 'react-native-maps';
+import { StyleSheet } from 'react-native';
+import Axios from 'axios'
 
 
-const Map = () => {
-  const styles = StyleSheet.create({
-    mapStyle: {
-      width: '100%',
-      height: '40%',
+const Map = (props) => {
+  const [cordinates, setCordinates] = useState({
+    latitude: 0,
+    longitude: 0
+  })
+
+  useEffect(() => {
+    if (props.address != null) {
+
+      Axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${props.address}&key=AIzaSyBYEECqPIORcLEuBpntcS-A9WVrFTOE23k`)
+        .then((response) => {
+          if (response.status == 200) {
+            setCordinates({
+              latitude: response.data.results[0].geometry.location.lat,
+              longitude: response.data.results[0].geometry.location.lng
+            });
+          }
+        })
     }
-  });
+  }, [props.address])
+
+  const getAddrees = (coordinate) => {
+    setCordinates(coordinate)
+    Axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${coordinate.latitude},${coordinate.longitude}&key=AIzaSyBYEECqPIORcLEuBpntcS-A9WVrFTOE23k`)
+      .then((response) => {
+        props.setAddress(response.data.results[0].formatted_address)
+      })
+  }
+
   return (
     <>
-      <MapView style={styles.mapStyle}></MapView>
+      <MapView style={styles.mapStyle} onPress={e => getAddrees(e.nativeEvent.coordinate)}>
+        <Marker draggable
+          coordinate={cordinates}
+        />
+      </MapView>
     </>
   )
 }
+
+
+const styles = StyleSheet.create({
+  mapStyle: {
+    width: '100%',
+    height: '40%',
+  }
+});
 
 export default Map
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,36 +1,53 @@
-import React from 'react'
-import { StyleSheet, View} from 'react-native';
+import React, { useState } from 'react'
+import { StyleSheet, View, Text} from 'react-native';
 import Map from '../components/Map'
 import RoundPicture from '../components/RoundPicture'
-import { TextInput , Button } from 'react-native-paper';
+import { TextInput , Button, Dialog } from 'react-native-paper';
 
 const Main = () => {
-
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 20,
-      backgroundColor: '#fff',
-      justifyContent: 'center',
-    },
-    textInput: {
-      margin: 20
-    },
-    buttom: {
-      height: 60,
-      justifyContent: 'center',
-    }
-  });
+  const [address, setAddress] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false)
   
   return (
     <View style={styles.container}>
       <RoundPicture/>
-      <TextInput  label='Endereço' style={styles.textInput} />
-      <Map/>
+      <TextInput  label='Endereço' style={styles.textInput} value={address} onFocus={() => {setModalVisible(true)}} onChangeText={text => setAddress(text)}/>
+      <Map address={address} setAddress={setAddress} />
       <TextInput  label='Numero de pessoas' style={styles.textInput}/>
       <Button onPress={() => alert()} mode="contained" style={styles.buttom}> Notificar autoridade </Button>
+
+      <Dialog
+        visible={modalVisible}
+        onDismiss={() => setModalVisible(false)}
+      >
+        <Dialog.Title>Atenção</Dialog.Title>
+          <Dialog.Content>
+            <Text>Voce deseja pegar a sua localização atual?</Text>
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button onPress={() => setModalVisible(false)}>Sim</Button>
+            <Button onPress={() => setModalVisible(false)}>Não</Button>
+          </Dialog.Actions>
+
+      </Dialog>
     </View>
   )
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+  },
+  textInput: {
+    margin: 10,
+    width: '95%'
+  },
+  buttom: {
+    height: 60,
+    justifyContent: 'center',
+  }
+});
 export default Main
